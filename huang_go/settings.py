d62f6536fe52faf8a03fe073fbbe850586a0db62
roberta nogara <r.nogara.dev@gmail.com>
2023-12-21 18:16:07 -0300

    fixes in db

--- huang_go/settings.py
+++ huang_go/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n1+1un8qgc=drio6jkc4#(p5ql7&t2utpo%+=d-3gpsu0%rm0c'
+SECRET_KEY = os.getenv('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -127,7 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Mail config
-load_dotenv()
 EMAIL_BACKEND = os.getenv('BACKEND')
 EMAIL_HOST = os.getenv('HOST')
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
